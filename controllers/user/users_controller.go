f5cc3e813799aa30bb0db1ede369f8b0c4c1f3d9
nyakundigeorge <gee.nyakundi@gmail.com>
2020-05-05 06:47:22 +0300

    feat(user-controller): updated the create user method

--- controllers/user/users_controller.go
+++ controllers/user/users_controller.go
@@ -1,6 +1,9 @@
 package user
 
 import (
+	"github.com/DevAgani/bookstore_users-api/domain/users"
+	"github.com/DevAgani/bookstore_users-api/services"
+	"github.com/DevAgani/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,7 +12,18 @@ func GetUser(c *gin.Context){
 	c.JSON(http.StatusNotImplemented,"implement me")
 }
 func CreateUser(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,"implement me")
+	var user users.User
+	if err := c.ShouldBindJSON(&user);err != nil{
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status,restErr)
+	}
+	result, saveError := services.CreateUser(user)
+	if saveError != nil{
+		// TODO Handle user creation error
+		c.JSON(saveError.Status, saveError)
+		return
+	}
+	c.JSON(http.StatusCreated,result)
 }
 func SearchUser(c *gin.Context){
 	c.JSON(http.StatusNotImplemented,"implement me")
