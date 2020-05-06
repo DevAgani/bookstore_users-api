9f013dbb33bd7bd9f4cabba8e4343adb8b00273a
nyakundigeorge <dev.things.ke@gmail.com>
2020-05-06 06:40:29 +0300

    feat(refactor dao): refactored data access object to use mysql utils package

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,43 +1,50 @@
 package users
 
 import (
-	"fmt"
 	"github.com/DevAgani/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/DevAgani/bookstore_users-api/utils/date_utils"
 	"github.com/DevAgani/bookstore_users-api/utils/errors"
+	"github.com/DevAgani/bookstore_users-api/utils/mysql_utils"
 )
 
-var (
-	usersDB = make(map[int64]*User)
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows = "no rows in result set"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser = "SELECT id, first_name, last_name, email, date_created FROM users where id=?;"
 )
+
 
 func (user *User) Get()  *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil{
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil{
+		return errors.NewInternalServerError(err.Error())
 	}
-	result := usersDB[user.Id]
-	if result == nil{
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found",user.Id))
+	defer stmt.Close()
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName,&user.LastName,&user.Email,&user.DateCreated);err != nil{
+		return mysql_utils.ParseError(err)
 	}
-	user.Id = result.Id
-	user.Email = result.Email
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.DateCreated = result.DateCreated
-
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr  {
-	current := usersDB[user.Id]
-	if current != nil{
-		if current.Email == user.Email{
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists",user.Id))
+	stmt, err  := users_db.Client.Prepare(queryInsertUser)
+	if err != nil{
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
-	usersDB[user.Id] = user
+
+	insertResult, insertErr := stmt.Exec(user.FirstName,user.LastName,user.Email,user.DateCreated)
+	if insertErr != nil{
+		return mysql_utils.ParseError(insertErr)
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil{
+		return mysql_utils.ParseError(insertErr)
+	}
+	user.Id = userId
 	return nil
 }
