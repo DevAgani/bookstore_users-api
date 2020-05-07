b16bc9108a3b3d16d22aa28239bf9248b71dd044
nyakundigeorge <dev.things.ke@gmail.com>
2020-05-07 07:11:31 +0300

    feat(dao): added the status and password fields

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,8 +1,8 @@
 package users
 
 import (
+	"fmt"
 	"github.com/DevAgani/bookstore_users-api/datasources/mysql/users_db"
-	"github.com/DevAgani/bookstore_users-api/utils/date_utils"
 	"github.com/DevAgani/bookstore_users-api/utils/errors"
 	"github.com/DevAgani/bookstore_users-api/utils/mysql_utils"
 )
@@ -10,10 +10,11 @@ import (
 const (
 	indexUniqueEmail = "email_UNIQUE"
 	errorNoRows = "no rows in result set"
-	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
-	queryGetUser = "SELECT id, first_name, last_name, email, date_created FROM users where id=?;"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created,status,password) VALUES(?, ?, ?, ?,?,?);"
+	queryGetUser = "SELECT id, first_name, last_name, email, date_created,status FROM users where id=?;"
 	queryUpdateUser = "UPDATE users SET first_name=?, last_name=?, email=? WHERE id=?;"
 	queryDeleteUser = "DELETE FROM users where id=?;"
+	queryFindUserByStatus = "SELECT id, first_name, last_name, email, date_created,status from users where status=?;"
 )
 
 
@@ -24,7 +25,7 @@ func (user *User) Get()  *errors.RestErr {
 	}
 	defer stmt.Close()
 	result := stmt.QueryRow(user.Id)
-	if err := result.Scan(&user.Id, &user.FirstName,&user.LastName,&user.Email,&user.DateCreated);err != nil{
+	if err := result.Scan(&user.Id, &user.FirstName,&user.LastName,&user.Email,&user.DateCreated,&user.Status);err != nil{
 		return mysql_utils.ParseError(err)
 	}
 	return nil
@@ -37,9 +38,7 @@ func (user *User) Save() *errors.RestErr  {
 	}
 	defer stmt.Close()
 
-	user.DateCreated = date_utils.GetNowString()
-
-	insertResult, insertErr := stmt.Exec(user.FirstName,user.LastName,user.Email,user.DateCreated)
+	insertResult, insertErr := stmt.Exec(user.FirstName,user.LastName,user.Email,user.DateCreated,user.Status,user.Password)
 	if insertErr != nil{
 		return mysql_utils.ParseError(insertErr)
 	}
@@ -75,4 +74,29 @@ func (user *User) Delete() *errors.RestErr{
 		return mysql_utils.ParseError(err)
 	}
 	return nil
+}
+
+func (user *User)FindByStatus(status string) ([]User, *errors.RestErr)  {
+	stmt, err := users_db.Client.Prepare(queryFindUserByStatus)
+	if err != nil{
+		return nil,errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+	rows,err := stmt.Query(status)
+	if err != nil{
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer rows.Close()
+	results := make([]User,0)
+	for rows.Next(){
+		var user User
+		if err := rows.Scan(&user.Id,&user.FirstName,&user.LastName,&user.Email,&user.DateCreated,&user.Status);err != nil{
+			return nil,mysql_utils.ParseError(err)
+		}
+		results = append(results, user)
+	}
+	if len(results)== 0{
+		return nil,errors.NewNotFoundError(fmt.Sprintf("no users matching status %s",status))
+	}
+	return results,nil
 }
--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -1,8 +1,13 @@
 package users
 
 import (
+	"fmt"
 	"github.com/DevAgani/bookstore_users-api/utils/errors"
 	"strings"
+)
+
+const (
+	ActiveStatus = "active"
 )
 
 type User struct {
@@ -11,10 +16,34 @@ type User struct {
 	LastName string `json:"last_name"`
 	Email string `json:"email"`
 	DateCreated string `json:"date_created"`
+	Status	string `json:"status"`
+	Password string `json:"password"`
 }
 
 func (user *User) Validate() *errors.RestErr  {
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == ""{
+		return errors.NewBadRequestError("invalid email address")
+	}
+	user.Password = strings.TrimSpace(user.Password)
+	if user.Password == ""{
+		return errors.NewBadRequestError("invalid password")
+	}
+	isPasswordValid := notValidatePassword(user.Password)
+	if !isPasswordValid{
+		return errors.NewBadRequestError(fmt.Sprintf("password should be more than 5 characters, you provided %d",len(user.Password)))
+	}
+
 	return nil
+}
+
+func notValidatePassword(pass string) bool{
+	if len(pass) < 5 {
+		return false
+	}
+	return true
 }
 
